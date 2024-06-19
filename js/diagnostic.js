48918a5353f7f73123a791bf180553295e86ed63
Muhammad Ainur Rofiq <rofiq@192.168.1.6>
2024-06-19 15:25:23 +0700

    m

--- js/diagnostic.js
+++ js/diagnostic.js
@@ -1,6 +1,37 @@
-let obat = {
-
-}
+let obat = [
+    {
+        nama: "Paramex",
+        harga: 4000,
+        stock: 28,
+        gejala: ['flu', 'pilek', 'demam'],
+        tokopediaLink: 'https://www.tokopedia.com/search?st=&q=panadol&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&navsource=',
+        gambarLink: 'https://down-id.img.susercontent.com/file/bf71b1b6417e71ed15f34911bce5647d'
+    },
+    {
+        nama: "Paramex",
+        harga: 4000,
+        stock: 28,
+        gejala: ['flu', 'pilek', 'demam'],
+        tokopediaLink: 'https://www.tokopedia.com/search?st=&q=panadol&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&navsource=',
+        gambarLink: 'https://down-id.img.susercontent.com/file/bf71b1b6417e71ed15f34911bce5647d'
+    },
+    {
+        nama: "Paramex",
+        harga: 4000,
+        stock: 28,
+        gejala: ['flu', 'pilek', 'demam'],
+        tokopediaLink: 'https://www.tokopedia.com/search?st=&q=panadol&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&navsource=',
+        gambarLink: 'https://down-id.img.susercontent.com/file/bf71b1b6417e71ed15f34911bce5647d'
+    },
+    {
+        nama: "Paramex",
+        harga: 4000,
+        stock: 28,
+        gejala: ['flu', 'pilek', 'demam'],
+        tokopediaLink: 'https://www.tokopedia.com/search?st=&q=panadol&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&navsource=',
+        gambarLink: 'https://down-id.img.susercontent.com/file/bf71b1b6417e71ed15f34911bce5647d'
+    },
+]
 
 // function (gejalaArr, database)
 
